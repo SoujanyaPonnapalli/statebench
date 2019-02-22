0798303fe6dea4f23e8121ecef4afb056a1b19e4
SoujanyaPonnapalli <soujanyap95@gmail.com>
2019-02-22 04:22:26 -0600

    Put: Throughput Latency

--- scripts/put.js
+++ scripts/put.js
@@ -18,7 +18,7 @@ const main = async (state, blockNum) => {
     const val = utils.ethAccountToRlp(data.value);
     if (state instanceof rainblock.MerklePatriciaTree) {
       state.put(key, val);
-    } else {
+    } else if (state) {
       let flag = false;
       state.put(key, val, () => {flag = true});
       wait.for.predicate(() => flag);
@@ -40,6 +40,7 @@ for (let blockNum = startBlock; blockNum <= endBlock; blockNum += interval) {
   const rstate = new rainblock.MerklePatriciaTree();
   const estate = new ethereumjs();
 
+  utils.addAsyncTest(suite, '--- ' + block, main, setup, null, blockNum);
   utils.addAsyncTest(suite, 'RBC ' + block, main, setup, rstate, blockNum);
   utils.addAsyncTest(suite, 'ETH ' + block, main, setup, estate, blockNum);
 }
